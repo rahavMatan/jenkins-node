ea4ea88e0649cb759c9d919c7e7c9371f6b905d9
rahavmatan <rahavmatan@gmail.com>
2017-07-19 13:44:48 +0300

    added hello world message

--- app.js
+++ app.js
@@ -2,7 +2,8 @@ var express = require('express');
 var app = express();
 
 app.get('/',function(req,res) {
-  res.send("Hello World!!");
+  res.send("Hello jenkins");
 });
 
 app.listen(4000);
+console.log('app is listening on port 4000');
